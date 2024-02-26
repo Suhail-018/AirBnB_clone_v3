65232ec4955d7309eda5dd4144e4a71c492c2fbd
suhail-09 <you@example.com>
2024-02-26 14:38:23 -0800

    modified

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Place route"""
 
-from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models import storage
 from models.place import Place
+from api.v1.views import app_views
 from models.city import City
 from models.user import User
 
